a7dc8b952e976cd9102ca08a6fb87eb25f8154b6
a.kalennyk <a.kalennyk@autodoc.eu>
2023-08-01 18:00:27 +0300

    Fix template

--- resources/views/service/partials/service-item.blade.php
+++ resources/views/service/partials/service-item.blade.php
@@ -3,28 +3,28 @@
         <a href="{{$service->getUrl()}}">
             <img src="{{$service->logo}}" class="card-img-top" alt="...">
         </a>
-        <div class="card-body">
+        <div class="card-body flex-column">
             <a href="{{$service->getUrl()}}">
                 <h5 class="card-title">{{$service->t('title')}}</h5>
             </a>
-            <div class="card-text d-flex">
+            <p class="card-text d-flex">
                 <span class="text-left">{{__t('Місто')}}</span>
-                <p class="ml-auto text-right text-primary">{{$service->city->t('title')}}</p>
-            </div>
-            <div class="card-text d-flex">
+                <span class="ml-auto text-right text-primary">{{$service->city->t('title')}}</span>
+            </p>
+            <p class="card-text d-flex">
                 <span class="text-left">{{__t('Адреса')}}</span>
-                <p class="ml-auto text-right text-primary">{!! $service->address !!}</p>
-            </div>
-            <div class="card-text d-flex">
+                <span class="ml-auto text-right text-primary">{!! $service->address !!}</span>
+            </p>
+            <p class="card-text d-flex">
                 <span class="text-left">{{__t('Телефон')}}</span>
-                <p class="ml-auto text-right text-primary">{!! $service->number !!}</p>
-            </div>
-            <div class="card-text d-flex justify-content-lg-end">
-                @if($service->site_url)
-                    <a href="{{$service->site_url}}" target="_blank" class="btn btn-primary py-2 mr-1 w-50">{{__t('Сайт Сервісу')}}</a>
-                @endif
-                <a href="{{$service->getUrl()}}" class="btn btn-secondary py-2 ml-1 w-50">{{__t('Детальніше')}}</a>
-            </div>
+                <span class="ml-auto text-right text-primary">{!! $service->number !!}</span>
+            </p>
+        </div>
+        <div class="d-flex mb-3 ml-2 mr-2">
+            @if($service->site_url)
+                <a href="{{$service->site_url}}" target="_blank" class="card-link btn btn-primary py-2 w-50">{{__t('Сайт Сервісу')}}</a>
+            @endif
+            <a href="{{$service->getUrl()}}" class="card-link btn btn-secondary py-2 w-50">{{__t('Детальніше')}}</a>
         </div>
     </div>
 </div>
