1c8d688d8c8ea98e131adb932028e482f7acb9ca
a.kalennyk <a.kalennyk@autodoc.eu>
2023-09-21 07:58:11 +0300

    Admin fixes

--- resources/views/home/partials/clubs.blade.php
+++ resources/views/home/partials/clubs.blade.php
@@ -9,20 +9,13 @@
         <div class="row carousel-local-club owl-carousel">
             @foreach($clubs as $club)
                     <div class="services services-2 w-100 text-center">
-                        <a href="{{$club->getExtraUrl()}}">
-                            <div class="icon d-flex align-items-center justify-content-center"
-                                @if($club->responsible)
-                                    data-toggle="tooltip"
-                                    title="Напиши відповідальному для отримання посилання"
-                                @endif
-                            >
-                                <img src="{{$club->getImgPath(640,640)}}" class="card-img img-cover rounded-circle shadow" alt="">
-                            </div>
-                            <div class="text w-100">
-                                <h3 class="heading mb-2">{{$club->city->t('title')}}</h3>
-                                <p></p>
-                            </div>
-                        </a>
+                        <div class="icon d-flex align-items-center justify-content-center">
+                            <img src="{{$club->getImgPath(640,640)}}" class="card-img img-cover rounded-circle shadow" alt="">
+                        </div>
+                        <div class="text w-100">
+                            <h3 class="heading mb-2">{{$club->city->t('title')}}</h3>
+                            <p></p>
+                        </div>
                     </div>
             @endforeach
         </div>
