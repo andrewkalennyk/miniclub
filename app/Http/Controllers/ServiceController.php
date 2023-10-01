1dda689ace680ca2a4577c2be614aae9abff5314
a.kalennyk <a.kalennyk@autodoc.eu>
2023-10-01 16:57:56 +0300

    Reviews page

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -20,7 +20,7 @@ class ServiceController extends TreeController
 
         $types = ServiceType::active()->get();
 
-        $services = Service::active()->with(['city', 'service_type', 'reviews'])->get();
+        $services = Service::active()->orderBy('mark','desc')->with(['city', 'service_type', 'reviews'])->get();
 
         return view('service.catalog', compact('page','types', 'services'));
     }
