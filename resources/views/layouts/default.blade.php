570c7d899d5f8ad31a5fa38d4579edfb6df5947e
andrewkalennyk <andrewkalennyk@gmail.com>
2023-11-21 21:02:55 +0200

    GTM

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -43,10 +43,21 @@
     <link rel="stylesheet" href="/css/addittional.css">
     @yield('additional_styles')
     @yield('seo_tags')
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N7CMSQKR');</script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N7CMSQKR"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div id="content_page">
     @include('partials.navbar')
     @yield('main')
