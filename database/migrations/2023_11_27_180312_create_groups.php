91a710f23ab0f3c1f0dac1d8722ce4268b792073
andrewkalennyk <andrewkalennyk@gmail.com>
2023-11-27 21:24:40 +0200

    car groups + models migration

---
+++ database/migrations/2023_11_27_180312_create_groups.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroups extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('car_models', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('car_group_id');
+            $table->string('title');
+            $table->string('title_en');
+            $table->tinyInteger('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('car_models');
+    }
+}
