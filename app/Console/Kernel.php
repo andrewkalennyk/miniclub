08085a5df3c96ef0490dbcd2e142eea149ee6476
andrewkalennyk <andrewkalennyk@gmail.com>
2024-04-27 07:47:09 +0200

    Interesting fact schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\FunFact;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('fun:fact')->daily()->everyMinute();
+        $schedule->command(FunFact::class)->everyMinute();
 
     }
 
