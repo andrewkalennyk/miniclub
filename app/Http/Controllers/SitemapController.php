ede26b03d6fbae2abe9a7937609ee9b0ff023d05
andrewkalennyk <andrewkalennyk@gmail.com>
2024-06-06 07:39:15 +0200

    Sitemap changes

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
 use App\Models\Tree;
 use Illuminate\Support\Carbon;
 use Illuminate\View\View;
@@ -19,12 +20,22 @@ class SitemapController extends Controller
 
         $tree = Tree::active()->orderBy('id', 'asc')->get();
 
+        $services = Service::active()->get();
 
         foreach ($tree as $treePage) {
+            $priority = $treePage->template == 'main' ? '1.0' : '0.8';
+            $frequency = $treePage->template == 'main' ? Url::CHANGE_FREQUENCY_WEEKLY : Url::CHANGE_FREQUENCY_MONTHLY;
             $sitemap->add(Url::create($treePage->getUrl())
                 ->setLastModificationDate(Carbon::yesterday())
-                ->setChangeFrequency(Url::CHANGE_FREQUENCY_YEARLY)
-                ->setPriority(0.1));
+                ->setChangeFrequency($frequency)
+                ->setPriority($priority));
+        }
+
+        foreach ($services as $service) {
+            $sitemap->add(Url::create($service->getUrl())
+                ->setLastModificationDate(Carbon::yesterday())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+                ->setPriority(0.7));
         }
 
         $sitemap->writeToFile(public_path('sitemap.xml'));
