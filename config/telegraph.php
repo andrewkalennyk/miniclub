b8626ad2ed8ac9e5356276ae5172b82d3beb1d54
andrewkalennyk <andrewkalennyk@gmail.com>
2024-06-13 14:36:23 +0200

    bot changes

--- config/telegraph.php
+++ config/telegraph.php
@@ -35,7 +35,7 @@ return [
      * If enabled, Telegraph dumps received
      * webhook messages to logs
      */
-    'debug_mode' => true,
+    'debug_mode' => false,
 
     /*
      * If enabled, unknown webhook commands are
@@ -47,12 +47,12 @@ return [
         /*
          * if enabled, allows callback queries from unregistered chats
          */
-        'allow_callback_queries_from_unknown_chats' => false,
+        'allow_callback_queries_from_unknown_chats' => true,
 
         /*
          * if enabled, allows messages and commands from unregistered chats
          */
-        'allow_messages_from_unknown_chats' => false,
+        'allow_messages_from_unknown_chats' => true,
 
         /*
          * if enabled, store unknown chats as new TelegraphChat models
