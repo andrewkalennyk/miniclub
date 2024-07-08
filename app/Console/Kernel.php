2085e25726ef47bb5d586047ca326411c39e8ed5
andrewkalennyk <andrewkalennyk@gmail.com>
2024-07-08 17:35:37 +0200

    Fun Fact command off

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(FunFact::class)->daily()->at('06:30');
+        /*$schedule->command(FunFact::class)->daily()->at('06:30');*/
 
     }
 
