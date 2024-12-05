a57dc535fa15f4825684caf1ce66b2288e30ef49
andrewkalennyk <andrewkalennyk@gmail.com>
2024-12-05 18:43:11 +0100

    Products

--- resources/views/shop/partials/item.blade.php
+++ resources/views/shop/partials/item.blade.php
@@ -1,6 +1,13 @@
 <div class="col-md-4 col-sm-6 col-xs-12 mb-3 align-items-stretch">
     <div class="card rounded ftco-animate border-0 shadow-sm h-100">
-        <img src="{{$product->getPictureUrl()}}" class="card-img-top card-img-top-20" alt="...">
+        <div class="card-img-wrapper d-flex align-items-center justify-content-center"
+             style="height: 200px; overflow: hidden;
+             ">
+            <img src="{{$product->getPictureUrl()}}"
+                 class="w-100 h-100"
+                 style="object-fit: contain;"
+                 alt="{{$product->t('title')}}">
+        </div>
         <div class="card-body pb-0">
             <h5 class="card-title font-weight-normal">{{$product->t('title')}}</h5>
             <div class="d-flex">
